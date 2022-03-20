0c6f3eb70efd101e847d92b2f61f7d30fb72a03e
nicolasluarte <l.luarte@uc.cl>
2022-03-20 16:05:12 -0300

    hours

--- fed_programs/FED_delay_control/src/headers.h
+++ fed_programs/FED_delay_control/src/headers.h
@@ -55,7 +55,7 @@ char timeStampString[30];
 char FEDLog[30];
 bool pauseDelivery = false;
 // remove hours when FED is being maintained
-int HH[] = {12, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24};
+int HH[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 0};
 int randomHoursSize = 6;
 int hourArray[6];
 
