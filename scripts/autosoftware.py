473ade97aeca435e990439fa079fe59b68d9db20
nicolasluarte <l.luarte@uc.cl>
2022-03-22 15:22:30 -0300

    fast and dirty auto software upload

---
+++ scripts/autosoftware.py
@@ -0,0 +1,14 @@
+import os
+import pyfiglet
+
+
+print(pyfiglet.figlet_format("FED CLOCK SYNC"))
+cmdClock = 'pio run -d ~/nbolab_FED/fed_programs/FED_set_clock -t upload'
+os.system(cmdClock)
+print(pyfiglet.figlet_format("CLOCK SYNC DONE!"))
+
+print(pyfiglet.figlet_format("FED DELAY CONTROL"))
+cmdDelayControl = 'pio run -d ~/nbolab_FED/fed_programs/FED_delay_control -t upload'
+os.system(cmdDelayControl)
+print(pyfiglet.figlet_format("DELAY CONTROL UPLOADED!"))
+
