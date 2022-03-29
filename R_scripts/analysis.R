807cc1a222ba1288d02b061c499d6a526449f1b2
nicolasluarte <l.luarte@uc.cl>
2022-03-29 10:19:25 -0300

    update

--- R_scripts/analysis.R
+++ R_scripts/analysis.R
@@ -4,12 +4,22 @@ pacman::p_load(tidyverse, cowplot, lme4, lmerTest, ggrepel)
 df <- readRDS("df.rds")
 
 # count pellets per day
-# manual_data <- tibble(animal = 322, date = "2022-03-22", pellets_consumed = 12)
 df %>%
-	filter(motorTurns != "01", date != "2022-03-27") %>%
+	filter(date < "2022-03-28") %>%
 	group_by(animal, date) %>%
 	summarise(pellets_consumed = n()) %>%
 	ungroup() -> pellets_consumed
+
+pellets_consumed %>%
+	filter(date > "2022-03-23", date < "2022-03-28") %>%
+	group_by(animal) %>%
+	summarise(mean_p = mean(pellets_consumed)) %>% arrange(desc(mean_p)) %>%
+	mutate(group = as.factor(c(1, 2, 1, 2, 1, 2, 1, 2))) -> group_ll
+a <- group_ll %>% filter(group == 1) %>% select(mean_p)
+b <- group_ll %>% filter(group == 2) %>% select(mean_p)
+t.test(a,b)
+
+
 # grand mean
 pellets_consumed %>%
 	group_by(date) %>%
