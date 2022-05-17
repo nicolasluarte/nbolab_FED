0197da6d503dccf592ea34f5b20ac782c991b3cc
nicolasluarte <l.luarte@uc.cl>
2022-05-17 13:26:55 -0400

    update

--- fed_programs/FED_delay_control/src/feed.ino
+++ fed_programs/FED_delay_control/src/feed.ino
@@ -1,19 +1,32 @@
 void feed() {
+  queryPI();
   if (!pellet) {
     displayFeed();
     for (int i = 0; i < 20 + random(0, 10); i++) {
+      queryPI();
       if (!pellet) {
         //myMotor->step(15, FORWARD, DOUBLE);
         //myMotor->step(45, BACKWARD, DOUBLE);
-        myMotor->step(7, FORWARD, DOUBLE);
-        myMotor->step(14, BACKWARD, DOUBLE);
+        myMotor->step(7, BACKWARD, DOUBLE);
+        myMotor->step(14, FORWARD, DOUBLE);
         delay(50);
-        queryPI();
       }
     }
     myMotor->release();
   }
   motorTurns++;
+ if (!pellet){
+   for (int i = 0; i < 10 + random(0, 10); i++) {
+      queryPI();
+      if (!pellet) {
+        //myMotor->step(15, FORWARD, DOUBLE);
+        //myMotor->step(45, BACKWARD, DOUBLE);
+        myMotor->step(1, FORWARD, DOUBLE);
+      }
+    }
+    myMotor->release();
+  }
+
   if (motorTurns > turnsBeforeClear){
     pelletJam = true;
   }
@@ -23,9 +36,21 @@ void feed() {
 }
 
 void clearJam(){
+  queryPI();
   if (!pellet){
-    for (int i = 0; i < 25 + random(0, 50); i++){
+    for (int i = 0; i < 40 + random(0, 10); i++){
       myMotor->step(i, FORWARD, DOUBLE);
+      queryPI();
+      if (pellet){
+        break; // if pellet was delivered after clearJam exit loop
+      }
+    }
+  }
+  delay(150);
+  queryPI();
+  if (!pellet){
+    for (int i = 0; i < 40 + random(0, 10); i++){
+      myMotor->step(i, BACKWARD, DOUBLE);
       queryPI();
       if (pellet){
         break; // if pellet was delivered after clearJam exit loop
