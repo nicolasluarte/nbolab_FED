74d3b11185a0ce410feff8cdd8d51b5fa0f1def5
nicolasluarte <l.luarte@uc.cl>
2022-06-01 14:44:57 -0400

    update

--- scripts/autolog.py
+++ scripts/autolog.py
@@ -12,9 +12,9 @@ os.system(showDevices)
 
 # auto choose sdb1 for now
 # later on implement a selection prompt
-mountDevices = 'sudo mount /dev/sdb1 ~/usb-drive'
-lsMountDevices = 'ls ~/usb-drive'
-lsExperimental = 'cat ~/usb-drive/config.txt'
+mountDevices = 'sudo mount /dev/sdb1 ~/drive'
+lsMountDevices = 'ls ~/drive'
+lsExperimental = 'cat ~/drive/config.txt'
 mountPrompt = input("Code to run  " + mountDevices + "(y/n) : ")
 
 if mountPrompt == 'y':
@@ -24,7 +24,7 @@ if mountPrompt == 'y':
     print("### EXPERIMENTAL PARAMETERS ###")
     os.system(lsExperimental)
     # load json file
-    f = open('/home/nicoluarte/usb-drive/config.txt').read()
+    f = open('/home/nicoluarte/drive/config.txt').read()
     pattern = re.compile("[0-9]{3}")
     animalNumber = pattern.search(f)[0]
     # animal number
@@ -33,7 +33,7 @@ if mountPrompt == 'y':
     createDirectory = 'mkdir -p ../raw_data/' + animalNumber
     os.system(createDirectory)
     # copy animal data .CSV into the folder
-    copyCSV = 'sudo cp ~/usb-drive/' + animalNumber + '.CSV' + ' ../raw_data/' + animalNumber + '/'
+    copyCSV = 'sudo cp ~/drive/' + animalNumber + '.CSV' + ' ../raw_data/' + animalNumber + '/'
     os.system(copyCSV)
     # git add file
     gitAdd = 'git add ../raw_data/' + animalNumber + '/'
