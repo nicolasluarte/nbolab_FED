d75d9c16ce7d3143f64b694fb9d58b5cb1730082
nicolasluarte <l.luarte@uc.cl>
2022-10-28 11:21:04 -0300

    update

--- fed_programs/FED_delay_control/src/randFeedHours.ino
+++ fed_programs/FED_delay_control/src/randFeedHours.ino
@@ -3,8 +3,7 @@ void randFeedHours()
   randomSeed(analogRead(0));
   for (int i=0; i<12; i++)
   {
-    int rn = random(0, 2);
-    feedRandHours[i] = rn;
-    feedRandMins[i] = rn;
+    feedRandHours[i] = random(0, 2);
+    feedRandMins[i] = random(0, 2);
   }
 }
